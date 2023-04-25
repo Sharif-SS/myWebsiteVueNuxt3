2ebf3fbe3e3e12e0784652f7f5913b25f3b92f49
Sharif <78062547+Sharif-SS@users.noreply.github.com>
2023-04-25 15:17:55 -0230

    contact scaling bug fix progress

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,65 +1,81 @@
+<script lang="ts">
+</script>
+
 <template>
     <v-app>
-        <v-container>
-    <v-row no-gutters>
-        <v-col >
-          <v-sheet class="pa-6 ma-2">
-            <v-img 
-            align-center
-            :width="600"
-            aspect-ratio="16/9"
-        class="imgStyles"        
-        src="/contact.jpg" 
-        lazy-src="/contactLazyLoad.jpg"
-        >
-        <template v-slot:placeholder>
-          <div class="d-flex align-center justify-center fill-height">
-            <v-progress-circular
-              color="grey-lighten-4"
-              indeterminate
-            ></v-progress-circular>
-          </div>
-        </template>
-
-        </v-img>
-          </v-sheet>
-        </v-col>
-        <v-col >
-            <v-sheet class="pa-2 ma-2">
-
-                    <v-row>
-                      <v-col cols="12">
-                        <h2>
-                          Contact Me
-                        </h2>
-                      </v-col>
-                    </v-row>
-                    <v-row >
-                      <v-col cols="12" md="6" >
-                        <div class="content">
-                          <form name="contact" action="/thank-you" netlify-honeypot="bot-field" method="post" netlify>
-                            <input type="hidden" name="form-name" value="contact" />
-                            <p class="hidden">
-                              <label>Don't add anything here <input name="bot-field"></label>
-                            </p>
-                            <v-text-field class="form-field" name="name" id="name" label="Name"></v-text-field>
-                            <v-text-field class="form-field" name="email" id="email" label="Email"></v-text-field>
-                            <v-textarea class="form-field" name="message" id="message" label="Message"></v-textarea>
-                            <v-btn color="#D8FBFD" class="form-button" type="submit">Send message</v-btn>
-                          </form>
-                        </div>
-                      </v-col>
-                    </v-row>
-
-            </v-sheet>
-          </v-col>
-        </v-row>
-  </v-container>
+       
+      <v-container>
+        <v-row no-gutters>
+            <v-col cols="6">
+              <v-sheet class="pa-6 ma-2">
+                <v-img 
+            
+                align-center
+                max-width="600"
+                aspect-ratio="1/1"
+                contain
+                class="imgStyles "
+                src="/contact.jpg" 
+                lazy-src="/contactLazyLoad.jpg"
+            >
+            <template v-slot:placeholder>
+              <div class="d-flex align-center justify-center fill-height">
+                <v-progress-circular
+                  color="grey-lighten-4"
+                  indeterminate
+                ></v-progress-circular>
+              </div>
+            </template>
+    
+            </v-img>
+              </v-sheet>
+            </v-col>
+            <v-col >
+                <v-sheet class="pa-2 ma-2">
+    
+                        <v-row>
+                          <v-col >
+                            <h2>
+                              Contact Me
+                            </h2>
+                          </v-col>
+                        </v-row>
+                        <v-row >
+                          <v-col  >
+                            <div class="content">
+                              <form name="contact" action="/thank-you" netlify-honeypot="bot-field" method="post" netlify>
+                                <input type="hidden" name="form-name" value="contact" />
+                                <p class="hidden">
+                                  <label>Don't add anything here <input name="bot-field"></label>
+                                </p>
+                                <v-text-field class="form-field" name="name" id="name" label="Name"></v-text-field>
+                                <v-text-field class="form-field" name="email" id="email" label="Email"></v-text-field>
+                                <v-textarea class="form-field" name="message" id="message" label="Message"></v-textarea>
+                                <v-btn color="#D8FBFD" class="form-button" type="submit">Send message</v-btn>
+                              </form>
+                            </div>
+                          </v-col>
+                        </v-row>
+    
+                </v-sheet>
+              </v-col>
+            </v-row>
+      </v-container>
   
   </v-app>
   </template>
 
   <style scoped>
+
+  @media (max-width: 480px) {
+    .mobileOnly {
+      width: 300px;
+    }
+  }
+  
+ 
+
+
   .content {
     padding: 16px;
     background-color: #f5f5f5;
