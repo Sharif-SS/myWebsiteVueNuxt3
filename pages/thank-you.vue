19467a16adbd97069a2edc3e9ec400dcb9ca567a
Sharif <78062547+Sharif-SS@users.noreply.github.com>
2023-04-27 14:49:28 -0230

    thank you page

--- pages/thank-you.vue
+++ pages/thank-you.vue
@@ -1,10 +1,27 @@
 <template>
     <v-app>
-       
-      <v-container>
-            <h1>Thank you!</h1> 
+      <v-container >
+        <v-row no-gutters>
+          <v-col>
+            <v-sheet class="pa-2 ma-2">
+              <h1>Thank you! =)</h1>
+              <h3>Here is an ancient video from 2007~</h3>
+              
+            </v-sheet>
+          </v-col>
+
+    
+          <v-responsive width="100%"></v-responsive>
+    
+          <v-col>
+            <v-sheet class="pa-2 ma-2">
+              <iframe width="320" height="315" src="https://www.youtube.com/embed/Qit3ALTelOo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </v-sheet>
+          </v-col>
+    
+ 
+        </v-row>
       </v-container>
-  
   </v-app>
   </template>
 
