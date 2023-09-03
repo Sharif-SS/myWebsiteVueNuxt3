f43e985d498c2d4accfde115f483c2edbf2c1024
Sharif <78062547+Sharif-SS@users.noreply.github.com>
2023-09-02 22:37:41 -0230

    portfolio update complete

--- components/portfolio.vue
+++ components/portfolio.vue
@@ -4,6 +4,98 @@
     <v-divider :thickness="4" inset color="error"></v-divider>
 
     <v-container fluid>
+      <v-row>
+        <v-col cols="12" sm="6" md="6">
+          <v-card elevation="3">
+
+
+            <v-carousel show-arrows="hover" height="300px" hide-delimiter-background cycle delimiter-icon="mdi-bird">
+              <v-carousel-item src="portfolio/EST.png"></v-carousel-item>
+
+              <v-carousel-item src="portfolio/NAIA.png"></v-carousel-item>
+            </v-carousel>
+
+            <v-card-title>WordPress and Vue 2 + BootstrapVue Project - <a href="https://engagecreative.ca/">Engage
+                Creative</a>:</v-card-title>
+
+            <v-btn class="ma-2" rounded="lg" color="#D8FBFD">
+              BootstrapVue
+              <v-tooltip activator="parent" location="top">With BootstrapVue you can build responsive, mobile-first, and
+                ARIA accessible projects on the web using Vue.js </v-tooltip>
+            </v-btn>
+
+            <v-btn class="ma-2" rounded="lg" color="#D8FBFD">
+              Vue.js
+              <v-tooltip activator="parent" location="top">Vue.js is an open-source model–view–viewmodel front end
+                JavaScript framework for building user interfaces and single-page applications.</v-tooltip>
+            </v-btn>
+            <v-btn class="ma-2" rounded="lg" color="#D8FBFD">
+              TypeScript
+              <v-tooltip activator="parent" location="top">TypeScript is a free and open-source programming language
+                developed by Microsoft that adds static typing with optional type annotations to JavaScript.</v-tooltip>
+            </v-btn>
+
+            <v-btn class="ma-2" rounded="lg" color="#D8FBFD">
+              CSS
+              <v-tooltip activator="parent" location="top">CSS is the language we use to style an HTML document. CSS
+                describes how HTML elements should be displayed.</v-tooltip>
+            </v-btn>
+
+            <v-btn class="ma-2" rounded="lg" color="#D8FBFD">
+              WordPress
+              <v-tooltip activator="parent" location="top">
+                WordPress is a web content management system.</v-tooltip>
+            </v-btn>
+
+            <v-card-text>East Coast Trail's <a href="https://eastcoasttrail.com/choose-a-path/">Choose a path page</a>,
+              KML files with path info imported into Waymark.
+              <a href="https://aquacultureportal.ca">NAIA</a> - Newfoundland Aquaculture Industry Association project,
+              updated BootstrapVue and Vue 2 code to create a
+              dynamic platform for efficient file management and display from Firebase. </v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col cols="12" sm="6" md="6">
+          <v-card elevation="3">
+
+            <v-carousel show-arrows="hover" height="300px" hide-delimiter-background cycle delimiter-icon="mdi-bird">
+              <v-carousel-item src="portfolio/nlfdrogues-min.png"></v-carousel-item>
+
+              <v-carousel-item src="portfolio/oceanicReleaf-min.png"></v-carousel-item>
+            </v-carousel>
+
+            <v-card-title>WordPress sites - <a href="https://gorobot.ca">Robot Interactive + Marketing</a>:
+            </v-card-title>
+
+            <v-btn class="ma-2" rounded="lg" color="#D8FBFD">
+              WordPress
+              <v-tooltip activator="parent" location="top">
+                WordPress is a web content management system.</v-tooltip>
+            </v-btn>
+
+            <v-btn class="ma-2" rounded="lg" color="#D8FBFD">
+              Divi Builder
+              <v-tooltip activator="parent" location="top">
+                Divi isn't just a WordPress theme, it's a complete design framework that allows you to design and
+                customize every part of your website from the ground up.</v-tooltip>
+            </v-btn>
+
+            <v-btn class="ma-2" rounded="lg" color="#D8FBFD">
+              CSS
+              <v-tooltip activator="parent" location="top">
+                CSS is the language we use to style an HTML document. CSS describes how HTML elements should be displayed.
+              </v-tooltip>
+            </v-btn>
+
+
+
+            <v-card-text>Notable pages during my time at Robot. NFLD Rogues (Slide 1) - <a
+                href="https://nfldrogues.ca/schedule/">Schedule page</a> and
+              Oceanic's new <a href="https://staging.oceanicreleaf.ca/locations/">location page</a> (Slide 2).
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
 
       <v-row>
 
@@ -11,7 +103,8 @@
 
           <v-card elevation="3">
             <v-img src="portfolio/dvgui-min.png" height="300px"></v-img>
-            <v-card-title>Data Visualization GUI - <a href="https://engagecreative.ca/">Engage Creative</a>:</v-card-title>
+            <v-card-title>Data Visualization GUI - <a href="https://engagecreative.ca/">Engage
+                Creative</a>:</v-card-title>
 
             <v-btn class="ma-2" rounded="lg" color="#D8FBFD">
               Vue.js
@@ -267,4 +360,5 @@
 </template>
 
 
-<style scoped> .v-row {}</style>
+<style scoped> .v-row {}
+</style>
