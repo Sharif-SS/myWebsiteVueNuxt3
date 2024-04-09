02f63f3a7260da71692bb6509557ed295f1c5858
Sharif <78062547+Sharif-SS@users.noreply.github.com>
2024-04-09 19:04:03 -0230

    Update portfolio.vue

--- components/portfolio.vue
+++ components/portfolio.vue
@@ -11,7 +11,7 @@
 
 
             <v-carousel show-arrows="hover" height="300px" hide-delimiter-background cycle delimiter-icon="mdi-bird">
-              <v-carousel-item src="/soon.jpg"></v-carousel-item>
+              <v-carousel-item src="portfolio/soon.jpg"></v-carousel-item>
 
               <v-carousel-item src="/puffin.png"></v-carousel-item>
             </v-carousel>
