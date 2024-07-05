ad12d0e3b3b1bd0c7af0ace0e77d13011d07f824
Sharif <78062547+Sharif-SS@users.noreply.github.com>
2024-07-05 20:17:27 -0230

    Title name change

--- components/Vehicles.vue
+++ components/Vehicles.vue
@@ -1,41 +1,26 @@
 <template>
 
     <VRow>
-        <VCol 
-        v-for="n in 5" :key="n"
-        cols="6"
-        sm="6"
-        md="6"
-        lg="3"
-        >
+        <VCol v-for="n in 5" :key="n" cols="6" sm="6" md="6" lg="3">
             <VHover>
                 <VCard>
 
-                    <vImg 
-                            :src="`/photos/Vehicles/image(${n}).jpg`"
-                            :lazy-src="`/photos/Vehicles/thumbnails/image(${n}).jpg`"
-                            aspect-ratio="1"
-                            transition="fab-transition"
-                            style="background: linear-gradient(rgb(245, 253, 255), rgba(0, 0, 0, 0.0))"
-                    >
+                    <vImg :src="`/photos/Vehicles/image(${n}).jpg`"
+                        :lazy-src="`/photos/Vehicles/thumbnails/image(${n}).jpg`" aspect-ratio="1"
+                        transition="fab-transition"
+                        style="background: linear-gradient(rgb(245, 253, 255), rgba(0, 0, 0, 0.0))">
 
-                    <template v-slot:placeholder>
+                        <template v-slot:placeholder>
 
-                        <VRow
-                            class="fill-height ma-0"
-                            align="center"
-                            justify="center">
+                            <VRow class="fill-height ma-0" align="center" justify="center">
 
-                            <VProgressCircular
-                                indeterminate
-                                color="grey-lighten-5"
-                            ></VProgressCircular>
+                                <VProgressCircular indeterminate color="grey-lighten-5"></VProgressCircular>
 
 
 
-                        </VRow>
+                            </VRow>
 
-                    </template>
+                        </template>
 
                     </vImg>
 
--- components/imageGallary.vue
+++ components/imageGallary.vue
@@ -12,31 +12,31 @@ import Events from '@/components/Events.vue';
 
 <template>
     <VCard class="ma-5">
-    <!-- each of these are enclosed in a row -->
-    
-    <h1 class="mt-4 mb-1 text-center">Portraits </h1>
-    <v-divider length="1750px" class="mb-6" :thickness="4" inset color="error"></v-divider>
-    <Portraits/>
+        <!-- each of these are enclosed in a row -->
 
-    <h1 class="mt-12 mb-1 text-center">Events + BTS</h1>
-    <v-divider length="1750px" class="mb-6" :thickness="4" inset color="error"></v-divider>
-    <Events/>
+        <h1 class="mt-4 mb-1 text-center">Portraits </h1>
+        <v-divider length="1750px" class="mb-6" :thickness="4" inset color="error"></v-divider>
+        <Portraits />
 
-    <h1 class="mt-12 mb-1 text-center">Nature & Outdoors</h1>
-    <v-divider length="1750px" class="mb-6" :thickness="4" inset color="error"></v-divider>
-    <Outdoors/>
+        <h1 class="mt-12 mb-1 text-center">Events + BTS</h1>
+        <v-divider length="1750px" class="mb-6" :thickness="4" inset color="error"></v-divider>
+        <Events />
 
-    <h1 class="mt-12 mb-1 text-center">Animals</h1>
-    <v-divider length="1750px" class="mb-6" :thickness="4" inset color="error"></v-divider>
-    <Animals/>
+        <h1 class="mt-12 mb-1 text-center">Nature & Outdoors</h1>
+        <v-divider length="1750px" class="mb-6" :thickness="4" inset color="error"></v-divider>
+        <Outdoors />
 
-    <h1 class="mt-12 mb-1 text-center">Vehicles</h1>
-    <v-divider length="1750px" class="mb-6" :thickness="4" inset color="error"></v-divider>
-    <Vehicles/>
+        <h1 class="mt-12 mb-1 text-center">Pets 🫎</h1>
+        <v-divider length="1750px" class="mb-6" :thickness="4" inset color="error"></v-divider>
+        <Animals />
 
-    <h1 class="mt-12 mb-1 text-center">Other</h1>
-    <v-divider length="1750px" class="mb-6" :thickness="4" inset color="error"></v-divider>
-    <Misc/>
+        <h1 class="mt-12 mb-1 text-center">Vehicles</h1>
+        <v-divider length="1750px" class="mb-6" :thickness="4" inset color="error"></v-divider>
+        <Vehicles />
+
+        <h1 class="mt-12 mb-1 text-center">Other</h1>
+        <v-divider length="1750px" class="mb-6" :thickness="4" inset color="error"></v-divider>
+        <Misc />
 
     </VCard>
 </template>
